265789321b3309b34998551dc3542e48c452dfec
unicornrainbow <antihero989@gmail.com>
2025-10-08 21:01:53 -0500

    add tailwind, build out basic views

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './views/Home'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -29,11 +27,7 @@ function App() {
     //     Click on the Vite and React logos to learn more
     //   </p>
     // </>
-    <>
-      <div>
-
-      </div>
-    </>
+    <Home/>
   )
 }
 
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -4,17 +4,17 @@ import TrayEditor from "./TrayEditor";
 export const Home = () => {
 
     return (
-        <div>
-            <div>
-                <div>
-                    <SideMenu/>
-                </div>
+        <div className="flex border h-full w-full">
+            <div className="flex border w-50 h-full">
+                <SideMenu/>
             </div>
-            <div>
-                <div>
-                    <span>Seedling Trays</span>
+            <div className="flex flex-col border h-full w-full">
+                <div className="border flex justify-center items-center h-25 w-full">
+                    <div>
+                        <span>Seedling Trays</span>
+                    </div>
                 </div>
-                <div>
+                <div className="border flex-1 w-full">
                     <TrayEditor/>
                 </div>
             </div>
